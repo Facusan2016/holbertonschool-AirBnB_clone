a136b071da93a55423cade4604fcccca405f7d8f
Facusan2016 <facusan2016@gmail.com>
2023-10-24 19:05:45 +0000

    fix: comment order

--- models/base_model.py
+++ models/base_model.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 
+"""This module defines the base_model class."""
+
 import uuid
 from datetime import datetime
-
-"""This module defines the base_model class."""
 
 
 class BaseModel:
