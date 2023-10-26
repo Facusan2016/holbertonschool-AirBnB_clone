8a7bcd6497bc57b89015a786890833c5ddad29aa
root <iamarabarrios457@gmail.com>
2023-10-26 10:39:14 -0700

    fix pycodestyle correction

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -4,12 +4,13 @@
 
 import json
 
+
 class FileStorage():
 
     """Declaration of base class"""
     __file_path = "file.json"
     __objects = {}
-    
+
     def all(self):
         """Method that returns all the objects"""
         return self.__objects
@@ -17,7 +18,7 @@ class FileStorage():
     def new(self, obj):
         """Method that adds a new object to the objects dict"""
         self.all()[f"{type(obj).__name__}.{obj.id}"] = obj
-    
+
     def save(self):
         """Method that serializes the objects dict and dumps it"""
         with open(self.__file_path, "w") as f:
@@ -25,7 +26,7 @@ class FileStorage():
             for k, obj in self.all().items():
                 obj_dict_repr[k] = obj.to_dict()
             json.dump(obj_dict_repr, f, indent=4)
-    
+
     def reload(self):
         """Method that reloads the serialized objects"""
         try:
@@ -40,13 +41,13 @@ class FileStorage():
                 from models.user import User
 
                 correspondences = {
-                    "Amenity" : Amenity,
-                    "BaseModel" : BaseModel,
-                    "City" : City,
-                    "Place" : Place,
-                    "Review" : Review,
-                    "State" : State,
-                    "User" : User
+                    "Amenity": Amenity,
+                    "BaseModel": BaseModel,
+                    "City": City,
+                    "Place": Place,
+                    "Review": Review,
+                    "State": State,
+                    "User": User
                 }
 
                 for key, obj in obj_dict.items():
